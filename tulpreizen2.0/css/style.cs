9f975d9a78408f3bf5c8917f5c1ddb70a7320693
joaquinAkAjuan <112825010+joaquinAkAjuan@users.noreply.github.com>
2024-11-05 08:28:44 +0100

    Add files via upload

---
+++ tulpreizen2.0/css/style.cs
@@ -0,0 +1,28 @@
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+  /*1rem = 10px*/
+  scroll-behavior: smooth;
+}
+
+a,
+a:visited,
+a:link {
+  color: #eee;
+  text-decoration: none;
+}
+
+body {
+  font-size: 2rem;
+  font-family: "Lato", sans-serif;
+  background: #3D246C;
+}
+
+/*# sourceMappingURL=style.cs.map */
